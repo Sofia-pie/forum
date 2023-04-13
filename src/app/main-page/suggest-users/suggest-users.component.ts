45b45c256552e86d60ec6666ab9a821470edf9f4
Sofiia_Piesnia <sophia.piesnia@gmail.com>
2023-04-13 19:00:19 +0300

    add suggestions menu

--- src/app/main-page/suggest-users/suggest-users.component.ts
+++ src/app/main-page/suggest-users/suggest-users.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-suggest-users',
   templateUrl: './suggest-users.component.html',
-  styleUrls: ['./suggest-users.component.css']
+  styleUrls: ['./suggest-users.component.css'],
 })
 export class SuggestUsersComponent implements OnInit {
+  faAdd = faPlus;
 
-  constructor() { }
+  suggestions = [
+    {
+      name: 'user1',
+      id: 1,
+    },
+    {
+      name: 'user3',
+      id: 3,
+    },
+    {
+      name: 'user2',
+      id: 2,
+    },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
