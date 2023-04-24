4bcee6cc4cb883198310c8946c250dfff73f07c7
Sofiia_Piesnia <sophia.piesnia@gmail.com>
2023-04-24 14:14:49 +0300

    add sort by upvotes

--- src/app/main-page/topics-list/topics-list.component.ts
+++ src/app/main-page/topics-list/topics-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { TagsService } from '../../core/services/tags.service';
+import { Tag } from '../../core/models/tag';
+import { Topic } from '../../core/models/topic';
 
 @Component({
   selector: 'app-topics-list',
@@ -7,7 +10,13 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TopicsListComponent implements OnInit {
   @Input() topics: any;
-  constructor() {}
+  tags: string[];
+  constructor(private tagsService: TagsService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.tagsService.getTags().subscribe((t) => (this.tags = t));
+  }
+  sortByUpvotes(): void {
+    this.topics.sort((a: Topic, b: Topic) => b.upvotes - a.upvotes);
+  }
 }
