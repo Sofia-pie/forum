166b811bb593c57cab4a43cdcd6fd9deae2df508
Sofiia_Piesnia <sophia.piesnia@gmail.com>
2023-04-24 10:47:55 +0300

    topic-page

---
+++ src/app/main-page/topic-page/topic-page.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  faArrowDown,
+  faArrowRight,
+  faArrowUp,
+  faComment,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import { TopicService } from '../../core/services/topic.service';
+import { Topic } from '../../core/models/topic';
+import { CommentsService } from '../../core/services/comments.service';
+import { Comment } from '../../core/models/comments';
+
+@Component({
+  selector: 'app-topic-page',
+  templateUrl: './topic-page.component.html',
+  styleUrls: ['./topic-page.component.css'],
+})
+export class TopicPageComponent implements OnInit {
+  faUp = faArrowUp;
+  faDown = faArrowDown;
+  faUser = faUser;
+  faRight = faArrowRight;
+  topic: Topic;
+  comments: Comment[];
+  comment: String;
+  constructor(
+    private route: ActivatedRoute,
+    private topicService: TopicService,
+    private commentsService: CommentsService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.topicService.getTopicById(Number(id)).subscribe((topic) => {
+      this.topic = topic;
+    });
+    this.commentsService
+      .getCommentsByTopicId(id!)
+      .subscribe((c) => (this.comments = c));
+  }
+
+  addComment() {
+    console.log(this.comment);
+  }
+}
